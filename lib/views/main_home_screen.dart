013fd7aeca1b6954f80455848e948319a917c378
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-05 15:45:06 +0600

    add bottom navbar

--- lib/views/main_home_screen.dart
+++ lib/views/main_home_screen.dart
@@ -10,17 +10,54 @@ class MainHomeScreen extends StatefulWidget {
 class _MainHomeScreenState extends State<MainHomeScreen> {
   @override
   Widget build(BuildContext context) {
+    int selectedIndex = 0;
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.add))
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 5,
+        iconSize: 32,
+        selectedItemColor: Colors.pink,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.black45,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn3.iconfinder.com/data/icons/feather-5/24/search-512.png",
+              height: 30,
+            ),
+            label: "Explore",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_border,
+            ),
+            label: "Wishlists",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn-icons-png.flaticon.com/512/2111/2111307.png",
+              height: 30,
+            ),
+            label: "Trip",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://static.vecteezy.com/system/resources/thumbnails/014/441/006/small_2x/chat-message-thin-line-icon-social-icon-set-png.png",
+              height: 30,
+            ),
+            label: "Message",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+              height: 30,
+            ),
+            label: "Profile",
+          ),
         ],
-        centerTitle: true,
-        title: Text("home"),
       ),
     );
   }
