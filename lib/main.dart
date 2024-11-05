07592cb719801f2d6ab8da1e4e8476d81982a13e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-05 17:17:39 +0600

    keep user login until logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:airbnb_app/firebase_options.dart';
 import 'package:airbnb_app/views/login_screen.dart';
+import 'package:airbnb_app/views/main_home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
   runApp(const MyApp());
 }
 
@@ -19,12 +20,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot){
+              if(snapshot.hasData){
+                return const MainHomeScreen();
+              }else{
+                return const LoginScreen();
+              }
+        })
+
+        // LoginScreen(),
+        );
   }
 }
