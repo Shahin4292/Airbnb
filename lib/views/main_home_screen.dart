f9df7327c470ad289f6537f94e51084dc955e571
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-05 15:58:06 +0600

    selected unselected color add

--- lib/views/main_home_screen.dart
+++ lib/views/main_home_screen.dart
@@ -8,9 +8,10 @@ class MainHomeScreen extends StatefulWidget {
 }
 
 class _MainHomeScreenState extends State<MainHomeScreen> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
@@ -22,17 +23,26 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
         selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
         unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
         type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             icon: Image.network(
               "https://cdn3.iconfinder.com/data/icons/feather-5/24/search-512.png",
               height: 30,
+              color: selectedIndex == 0 ? Colors.pinkAccent : Colors.black45,
             ),
             label: "Explore",
+
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(
               Icons.favorite_border,
+              color: selectedIndex == 1 ? Colors.pinkAccent : Colors.black45,
             ),
             label: "Wishlists",
           ),
@@ -40,6 +50,7 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
             icon: Image.network(
               "https://cdn-icons-png.flaticon.com/512/2111/2111307.png",
               height: 30,
+              color: selectedIndex == 2 ? Colors.pinkAccent : Colors.black45,
             ),
             label: "Trip",
           ),
@@ -47,6 +58,7 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
             icon: Image.network(
               "https://static.vecteezy.com/system/resources/thumbnails/014/441/006/small_2x/chat-message-thin-line-icon-social-icon-set-png.png",
               height: 30,
+              color: selectedIndex == 2 ? Colors.pinkAccent : Colors.black45,
             ),
             label: "Message",
           ),
@@ -54,6 +66,7 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
             icon: Image.network(
               "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
               height: 30,
+              color: selectedIndex == 2 ? Colors.pinkAccent : Colors.black45,
             ),
             label: "Profile",
           ),
